20466facd9e37fc5ed9f1f3ccdfe5d65b330a368
David <100006394.joan23@fje.edu>
2021-12-13 16:19:56 +0100

    cabecera.html

---
+++ processes/usuaris.php
@@ -0,0 +1,15 @@
+<?php
+class Evento{
+    public $id_usuari;
+    public $nom_usuari;
+    public $cognom_usuari;
+    public $contra_usuari;
+    public $tipus_usuari;
+    
+    function __construct($id_usuari,$nom_usuari,$cognom_usuari,$contra_usuari,$tipus_usuari){
+        $this->id_usuari=$id_usuari;
+        $this->nom_usuari=$nom_usuari;
+        $this->cognom_usuari=$cognom_usuari;
+        $this->contra_usuari=$contra_usuari;
+        $this->tipus_usuari=$tipus_usuari;
+}
